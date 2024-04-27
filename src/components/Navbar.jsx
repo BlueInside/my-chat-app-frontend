085cefd45630d29deed77d4820a0a14db13ad3df
Karol <kpulawski13@gmail.com>
2024-04-27 16:16:44 +0100

    Implement basic navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img src="#" alt="logo" />
+        </li>
+        <li>Home</li>
+        <li>Features</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
