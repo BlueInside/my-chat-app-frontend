ea105e37f0b6effd2c4771a2ab12a952546110e4
Karol <kpulawski13@gmail.com>
2024-05-06 11:33:47 +0100

    Create tests for header component

---
+++ src/tests/Header.test.jsx
@@ -0,0 +1,25 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import Header from '../components/Header';
+
+describe('Header component', () => {
+  it('Checks snapshot', () => {
+    const { container } = render(<Header></Header>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('Renders the header with main, call to action and buttons to log in or register', () => {
+    render(<Header></Header>);
+    const header = screen.getByRole('banner');
+    const loginButton = screen.getByRole('button', { name: /login/i });
+    const registerButton = screen.getByRole('button', { name: /register/i });
+    const actionHeading = within(header).getByRole('heading', {
+      name: /Chat Without Boundaries/i,
+    });
+
+    expect(header).toBeInTheDocument();
+    expect(actionHeading).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
+});
