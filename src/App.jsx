182bb201b8fc9cc18521db231eaecc75277412a9
Karol <kpulawski13@gmail.com>
2024-05-06 14:37:36 +0100

    Change app.js to use createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ChatPage from './pages/ChatPage';
-import ProfilePage from './pages/ProfilePage';
-import SettingsPage from './pages/SettingsPage';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import routesConfig from './router';
+
+const router = createBrowserRouter(routesConfig);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/chat/:conversationId" element={<ChatPage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
