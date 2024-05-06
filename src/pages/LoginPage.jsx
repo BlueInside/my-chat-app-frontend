16e2afe6b0de95acde39222e81a35aae10fad87c
Karol <kpulawski13@gmail.com>
2024-05-06 14:38:11 +0100

    Change mock content to display LoginForm

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,11 @@
+import LoginForm from '../components/LoginForm';
+
 function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
 
 export default LoginPage;
