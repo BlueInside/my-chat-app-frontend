c24bf8cc1763351f675cb70f197bdb73691a47fc
Karol <kpulawski13@gmail.com>
2024-05-06 12:17:42 +0100

    Create test for Footer component

---
+++ src/tests/Footer.test.jsx
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest';
+import Footer from '../components/Footer';
+import { render, screen, within } from '@testing-library/react';
+
+describe('Footer component', () => {
+  it('Render footer with github link and github icon', () => {
+    render(<Footer></Footer>);
+
+    const footer = screen.getByRole('contentinfo');
+    const githubLink = within(footer).getByRole('link');
+    const footerText = within(footer).getByText(/Developed by Karol Pulawski/i);
+
+    expect(footer).toBeInTheDocument();
+    expect(githubLink).toBeInTheDocument();
+    expect(footerText).toBeInTheDocument();
+  });
+});
