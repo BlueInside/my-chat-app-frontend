01d86546ef57fd929b11c4fa51287d70c5777666
Karol <kpulawski13@gmail.com>
2024-05-06 16:10:41 +0100

    Test that RegistrationPrompt renders correctly

---
+++ src/tests/RegistrationPrompt.test.jsx
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import RegistrationPrompt from '../components/RegistrationPrompt';
+
+describe('RegistrationPrompt component', () => {
+  it('Renders RegistrationPrompt correctly', () => {
+    render(
+      <MemoryRouter>
+        <RegistrationPrompt />
+      </MemoryRouter>
+    );
+
+    const paraText = screen.getByText(/not having account yet/i);
+    const registerButton = screen.getByRole('button', { name: /register/i });
+
+    expect(paraText).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
+});
