49b9b37eaf30d302b6d4a08b07aee6eb65a7ea6d
Karol <kpulawski13@gmail.com>
2024-05-06 16:11:38 +0100

    Test form inputs if save correct values

--- src/tests/LoginForm.test.jsx
+++ src/tests/LoginForm.test.jsx
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+
 import routesConfig from '../router';
 import LoginForm from '../components/LoginForm';
 
@@ -18,8 +20,29 @@ describe('LoginForm component', () => {
 
     const usernameInput = screen.getByLabelText(/username/i);
     const passwordInput = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole('button', { name: /login/i });
 
     expect(usernameInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('Form inputs works correctly', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <RouterProvider router={router}>
+        <LoginForm />
+      </RouterProvider>
+    );
+
+    const usernameInput = screen.getByLabelText(/username/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    await user.type(usernameInput, 'testUser');
+    await user.type(passwordInput, 'securePassword');
+
+    expect(usernameInput.value).toBe('testUser');
+    expect(passwordInput.value).toBe('securePassword');
   });
 });
