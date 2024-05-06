f9f8aac8577844181f54187731cf6d673249c52d
Karol <kpulawski13@gmail.com>
2024-05-06 12:27:10 +0100

    Add basic styles using styled-components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.nav`
+  background: #fff;
+  padding: 10px 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  height: 40px;
+`;
 
 function Navbar() {
   return (
-    <nav>
-      <img src="#" alt="logo" />
+    <StyledNavbar>
+      <Logo src="#" alt="logo" />
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -15,7 +29,7 @@ function Navbar() {
           <Link to="about">About</Link>
         </li>
       </ul>
-    </nav>
+    </StyledNavbar>
   );
 }
 
