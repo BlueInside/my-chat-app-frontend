1320714476ce4bb3b898abf998f18252076ff604
Karol <kpulawski13@gmail.com>
2024-05-06 12:17:27 +0100

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { FaGithub } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer>
+      <p>Developed by Karol Pulawski</p>
+
+      <a href="https://github.com/BlueInside">
+        <FaGithub size="2em" />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
