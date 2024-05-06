453b23438103d78606d0210e65d0584985710862
Karol <kpulawski13@gmail.com>
2024-05-06 16:09:58 +0100

    Change inputs to be controlled

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,46 @@
+import { useState } from 'react';
 import { Form } from 'react-router-dom';
 
 function LoginForm() {
+  const [formInputs, setFormInputs] = useState({ password: '', username: '' });
+
+  function onInputChange(inputName, value) {
+    setFormInputs({ ...formInputs, [inputName]: value });
+  }
+
   return (
     <Form>
-      <label htmlFor="username">Username:</label>
-      <input id="username" name="username" type="text" required />
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          value={formInputs.username}
+          onChange={(event) => {
+            onInputChange(event.target.name, event.target.value);
+          }}
+          required
+        />
+      </div>
 
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={formInputs.password}
+          onChange={(event) => {
+            onInputChange(event.target.name, event.target.value);
+          }}
+          required
+        />
+      </div>
 
-      <button type="submit">Login</button>
+      <div>
+        <button type="submit">Login</button>
+      </div>
     </Form>
   );
 }
