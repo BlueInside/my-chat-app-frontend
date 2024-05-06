ad2757d87e87a39095b71ac5fe0b35b205754502
Karol <kpulawski13@gmail.com>
2024-05-06 12:46:30 +0100

    Use BrowserRouter

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -1,15 +1,23 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import Header from '../components/Header';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 describe('Header component', () => {
   it('Checks snapshot', () => {
-    const { container } = render(<Header></Header>);
+    const { container } = render(
+      <Router>
+        <Header></Header>
+      </Router>
+    );
     expect(container).toMatchSnapshot();
   });
 
   it('Renders the header with main, call to action and buttons to log in or register', () => {
-    render(<Header></Header>);
+    render(
+      <Router>
+        <Header></Header>
+      </Router>
+    );
     const header = screen.getByRole('banner');
     const loginButton = screen.getByRole('button', { name: /login/i });
     const registerButton = screen.getByRole('button', { name: /register/i });
