0c539315c9fb33dcb62df81b569921b00279530f
Karol <kpulawski13@gmail.com>
2024-05-06 11:33:32 +0100

    Create simple header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <h1>Chat Without Boundaries</h1>
+      <div>
+        <button>Login</button>
+        <button>register</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
