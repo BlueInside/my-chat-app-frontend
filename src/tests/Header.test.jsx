4c01c9dda42cc683372df8965d7f048328fc4e15
Karol <kpulawski13@gmail.com>
2024-05-07 19:55:10 +0100

    Test AuthContext

--- src/tests/Header.test.jsx
+++ src/tests/Header.test.jsx
@@ -1,7 +1,9 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import Header from '../components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext';
+
 describe('Header component', () => {
   it('Checks snapshot', () => {
     const { container } = render(
@@ -29,5 +31,32 @@ describe('Header component', () => {
     expect(actionHeading).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
     expect(registerButton).toBeInTheDocument();
+  });
+
+  it(`Does not show login or register buttons when user is logged in`, () => {
+    const mockUser = {
+      id: '123',
+      username: 'testUser',
+      token: 'token',
+    };
+
+    render(
+      <Router>
+        <AuthContext.Provider value={{ user: mockUser, logout: vi.fn() }}>
+          <Header></Header>
+        </AuthContext.Provider>
+      </Router>
+    );
+    const loginButton = screen.queryByRole('button', { name: /login/i });
+    const registerButton = screen.queryByRole('button', { name: /register/i });
+    const actionHeading = screen.queryByRole('heading', {
+      name: /Chat Without Boundaries/i,
+    });
+    const userName = screen.getByText(/testUser/i);
+
+    expect(actionHeading).not.toBeInTheDocument();
+    expect(loginButton).not.toBeInTheDocument();
+    expect(registerButton).not.toBeInTheDocument();
+    expect(userName).toBeInTheDocument();
   });
 });
