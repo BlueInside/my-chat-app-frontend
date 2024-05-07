b61059d994695beabbd1bf257a5d999bef31a4b1
Karol <kpulawski13@gmail.com>
2024-05-07 10:11:58 +0100

    Add test that checks if errors are displayed correctly

--- src/tests/LoginForm.test.jsx
+++ src/tests/LoginForm.test.jsx
@@ -1,10 +1,11 @@
 import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 
 import routesConfig from '../router';
 import LoginForm from '../components/LoginForm';
+import { act } from 'react';
 
 describe('LoginForm component', () => {
   const router = createMemoryRouter(routesConfig, {
@@ -44,5 +45,31 @@ describe('LoginForm component', () => {
 
     expect(usernameInput.value).toBe('testUser');
     expect(passwordInput.value).toBe('securePassword');
+  });
+
+  it('displays error message on invalid login', async () => {
+    const user = userEvent.setup();
+    render(
+      <RouterProvider router={router}>
+        <LoginForm />
+      </RouterProvider>
+    );
+
+    const usernameInput = screen.getByLabelText(/username/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+
+    await user.type(usernameInput, 'wronguser');
+    await user.type(passwordInput, 'wrongpass');
+
+    await act(async () => {
+      await user.click(loginButton);
+    });
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/incorrect username or password/i)
+      ).toBeInTheDocument();
+    });
   });
 });
