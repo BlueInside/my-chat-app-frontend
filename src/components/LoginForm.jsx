083c1df08efd0b4a13d96eb7c54a7e75ff8c20a9
Karol <kpulawski13@gmail.com>
2024-05-07 10:10:38 +0100

    Add login header

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -10,40 +10,44 @@ function LoginForm() {
   }
 
   return (
-    <Form action="/login" method="POST">
-      <div>{errorData && <p>{errorData.error}</p>}</div>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input
-          id="username"
-          name="username"
-          type="text"
-          value={formInputs.username}
-          onChange={(event) => {
-            onInputChange(event.target.name, event.target.value);
-          }}
-          required
-        />
-      </div>
+    <>
+      <h1>Login</h1>
 
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          value={formInputs.password}
-          onChange={(event) => {
-            onInputChange(event.target.name, event.target.value);
-          }}
-          required
-        />
-      </div>
+      <Form action="/login" method="POST">
+        <div>{errorData && <p>{errorData.error}</p>}</div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={formInputs.username}
+            onChange={(event) => {
+              onInputChange(event.target.name, event.target.value);
+            }}
+            required
+          />
+        </div>
 
-      <div>
-        <button type="submit">Login</button>
-      </div>
-    </Form>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={formInputs.password}
+            onChange={(event) => {
+              onInputChange(event.target.name, event.target.value);
+            }}
+            required
+          />
+        </div>
+
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </Form>
+    </>
   );
 }
 
