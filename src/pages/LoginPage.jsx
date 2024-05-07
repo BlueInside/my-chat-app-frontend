f116ac437cd6c8a9e9c8af4503db562983310d15
Karol <kpulawski13@gmail.com>
2024-05-07 20:06:41 +0100

    Navigate user to chat if logged in

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,13 @@
+import { Navigate } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import Navbar from '../components/Navbar';
 import RegistrationPrompt from '../components/RegistrationPrompt';
+import { useAuth } from '../contexts/AuthContext';
 
 function LoginPage() {
+  const { user } = useAuth();
+  if (user) return <Navigate to={'/chat'} />;
+
   return (
     <>
       <Navbar />
