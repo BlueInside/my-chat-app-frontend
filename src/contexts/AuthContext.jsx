45b068fcd98cf0c3591014aac67d8d505b0dbfc8
Karol <kpulawski13@gmail.com>
2024-05-07 18:50:08 +0100

    Create Authorization context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const AuthContext = createContext({});
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    let source = axios.CancelToken.source();
+
+    const authenticateUser = async () => {
+      try {
+        const url = 'http://localhost:3000/authenticate/verify-token';
+        const token = localStorage.getItem('token');
+        if (!token) {
+          return;
+        }
+
+        const response = await axios.get(url, {
+          cancelToken: source.token,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          const userData = { ...response.data.user, token };
+          setUser(userData);
+        }
+      } catch (error) {
+        if (error.code === 'ERR_CANCELED') return;
+        console.error('Error during validation:', error.message);
+      }
+    };
+    authenticateUser();
+
+    return () => {
+      source.cancel('Cancelling in cleanup');
+    };
+  }, []);
+
+  const login = (userData) => {
+    localStorage.setItem('token', userData.token);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setUser(null);
+  };
+
+  const isAuthenticated = () => {
+    return !!localStorage.getItem('token');
+  };
+
+  const value = {
+    user,
+    login,
+    logout,
+    isAuthenticated,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
