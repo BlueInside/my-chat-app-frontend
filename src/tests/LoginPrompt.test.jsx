b145165433bc1d163595b8fc6b745eff25aae845
Karol <kpulawski13@gmail.com>
2024-05-07 10:12:11 +0100

    Create test for LoginPrompt component

---
+++ src/tests/LoginPrompt.test.jsx
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import LoginPrompt from '../components/LoginPrompt';
+
+describe('RegistrationPrompt component', () => {
+  it('Renders RegistrationPrompt correctly', () => {
+    render(
+      <MemoryRouter>
+        <LoginPrompt />
+      </MemoryRouter>
+    );
+
+    const paraText = screen.getByText(/Already a member/i);
+    const registerButton = screen.getByRole('button', { name: /login/i });
+
+    expect(paraText).toBeInTheDocument();
+    expect(registerButton).toBeInTheDocument();
+  });
+});
