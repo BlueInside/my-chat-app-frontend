e3978c0e9337ce0da2e80662e75985eacb8378ef
Karol <kpulawski13@gmail.com>
2024-05-09 11:52:57 +0100

    Implement basic ChatPage component

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,6 +1,24 @@
+import { Form } from 'react-router-dom';
+
 function ChatPage() {
-    return <div>ChatPage</div>;
-  }
-  
-  export default ChatPage;
-  
+  return (
+    <>
+      <div>
+        <Form id="search-form" role="search">
+          <input
+            id="q"
+            aria-label="Search conversation"
+            placeholder="Search"
+            type="search"
+            name="q"
+          />
+          <div id="search-spinner" aria-hidden hidden={true}></div>
+        </Form>
+      </div>
+
+      <div role="list"></div>
+    </>
+  );
+}
+
+export default ChatPage;
