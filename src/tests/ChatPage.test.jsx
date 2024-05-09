40622e59795d04e8cd8429c0f5f8c61e5d8e4211
Karol <kpulawski13@gmail.com>
2024-05-09 20:21:37 +0100

    Write test for ChatPage

--- src/tests/ChatPage.test.jsx
+++ src/tests/ChatPage.test.jsx
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import ChatPage from '../pages/ChatPage';
 import { render, screen, waitFor } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
@@ -7,44 +7,59 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
 let mock = new MockAdapter(axios);
+let mockLoaderResponse = mockConversation;
+
+const mockConversationLoader = vi.fn(() => Promise.resolve(mockLoaderResponse));
 
 describe('ChatPage component', () => {
   let router;
 
   beforeEach(() => {
     mock.reset();
-
+    // reset mock loader response to mockConversation
+    mockLoaderResponse = mockConversation;
     router = createMemoryRouter(
       [
         {
           path: '/chat',
           element: <ChatPage />,
+          loader: mockConversationLoader,
         },
       ],
       { initialEntries: ['/chat'] }
     );
   });
 
-  it('Should display list conversation list', () => {
-    render(<RouterProvider router={router} />);
-
-    expect(screen.getByLabelText('Search conversation')).toBeInTheDocument();
-    expect(screen.getByRole('list')).toBeInTheDocument();
-  });
-
-  it('Should display conversation properly', async () => {
-    mock
-      .onGet('http://localhost:3000/conversations')
-      .reply(200, mockConversation);
+  it('Should display custom text if no conversations started', async () => {
+    router = createMemoryRouter(
+      [
+        {
+          path: '/chat',
+          element: <ChatPage />,
+          loader: () => Promise.resolve([]), // Return empty array
+        },
+      ],
+      { initialEntries: ['/chat'] }
+    );
 
     render(<RouterProvider router={router} />);
 
     await waitFor(() => {
+      expect(
+        screen.getByText('Yours conversations will be displayed here')
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('Should display conversation properly', async () => {
+    render(<RouterProvider router={router} />);
+
+    await waitFor(() => {
       const conversationName = screen.getByText(
-        mockConversation.participants[0].username
+        mockConversation[0].participants[0].username
       );
       const conversationLastMessage = screen.getByText(
-        mockConversation.lastMessage
+        mockConversation[0].lastMessage
       );
 
       expect(conversationName).toBeInTheDocument();
