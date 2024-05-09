f70a922cb3d0aa536ee13f09ca0b862648422ee7
Karol <kpulawski13@gmail.com>
2024-05-09 11:53:08 +0100

    Test for ChatPage

--- src/tests/ChatPage.test.jsx
+++ src/tests/ChatPage.test.jsx
@@ -1,12 +1,27 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import ChatPage from '../pages/ChatPage';
 import { render, screen } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 describe('ChatPage component', () => {
-  it('Should display list conversation list', () => {
-    render(<ChatPage />);
+  let router;
 
-    expect(screen.getByLabelText('search-bar')).toBeInTheDocument();
-    expect(screen.getByRole('conversation-list')).toBeInTheDocument();
+  beforeEach(() => {
+    router = createMemoryRouter(
+      [
+        {
+          path: '/chat',
+          element: <ChatPage />,
+        },
+      ],
+      { initialEntries: ['/chat'] }
+    );
+  });
+
+  it('Should display list conversation list', () => {
+    render(<RouterProvider router={router} />);
+
+    expect(screen.getByLabelText('Search conversation')).toBeInTheDocument();
+    expect(screen.getByRole('list')).toBeInTheDocument();
   });
 });
