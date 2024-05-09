4318581dab74ca0b01a088e3fef4ba338c431baf
Karol <kpulawski13@gmail.com>
2024-05-09 17:49:23 +0100

    Test if ChatPage renders correctly

--- src/tests/ChatPage.test.jsx
+++ src/tests/ChatPage.test.jsx
@@ -1,12 +1,19 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import ChatPage from '../pages/ChatPage';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import { mockConversation } from './mocks/mockConversation.js';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+let mock = new MockAdapter(axios);
 
 describe('ChatPage component', () => {
   let router;
 
   beforeEach(() => {
+    mock.reset();
+
     router = createMemoryRouter(
       [
         {
@@ -23,5 +30,25 @@ describe('ChatPage component', () => {
 
     expect(screen.getByLabelText('Search conversation')).toBeInTheDocument();
     expect(screen.getByRole('list')).toBeInTheDocument();
+  });
+
+  it('Should display conversation properly', async () => {
+    mock
+      .onGet('http://localhost:3000/conversations')
+      .reply(200, mockConversation);
+
+    render(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      const conversationName = screen.getByText(
+        mockConversation.participants[0].username
+      );
+      const conversationLastMessage = screen.getByText(
+        mockConversation.lastMessage
+      );
+
+      expect(conversationName).toBeInTheDocument();
+      expect(conversationLastMessage).toBeInTheDocument();
+    });
   });
 });
