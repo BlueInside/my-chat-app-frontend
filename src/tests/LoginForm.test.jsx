935f78a38f357dd4c309b569a6de1a0ed600a544
Karol <kpulawski13@gmail.com>
2024-05-09 09:21:24 +0100

    Add axios-mock-adapter and test if login works correctly

--- src/tests/LoginForm.test.jsx
+++ src/tests/LoginForm.test.jsx
@@ -1,11 +1,18 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
-
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import routesConfig from '../router';
 import LoginForm from '../components/LoginForm';
-import { act } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+
+// Setup axios mock
+let mock = new MockAdapter(axios);
+beforeEach(() => {
+  mock.reset();
+});
 
 describe('LoginForm component', () => {
   const router = createMemoryRouter(routesConfig, {
@@ -26,6 +33,44 @@ describe('LoginForm component', () => {
     expect(usernameInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
+  });
+
+  it('handles login correctly with valid credentials', async () => {
+    const user = userEvent.setup();
+    const loginMock = vi.fn();
+
+    mock.onPost('http://localhost:3000/authenticate/login').reply(200, {
+      user: { username: 'fakeUser' },
+      token: 'fake-token',
+    });
+
+    render(
+      <AuthContext.Provider
+        value={{
+          login: loginMock,
+        }}
+      >
+        <RouterProvider router={router}>
+          <LoginForm />
+        </RouterProvider>
+      </AuthContext.Provider>
+    );
+
+    const usernameInput = screen.getByLabelText(/username/i);
+    const passwordInput = screen.getByLabelText(/password/i);
+    const loginButton = screen.getByRole('button', { name: /login/i });
+
+    await user.type(usernameInput, 'validUser');
+    await user.type(passwordInput, 'validPass');
+
+    await user.click(loginButton);
+
+    await waitFor(() => {
+      expect(loginMock).toHaveBeenCalledWith({
+        user: { username: 'fakeUser' },
+        token: 'fake-token',
+      });
+    });
   });
 
   it('Form inputs works correctly', async () => {
@@ -49,6 +94,7 @@ describe('LoginForm component', () => {
 
   it('displays error message on invalid login', async () => {
     const user = userEvent.setup();
+    mock.onPost('http://localhost:3000/authenticate/login').reply(400);
     render(
       <RouterProvider router={router}>
         <LoginForm />
@@ -62,9 +108,7 @@ describe('LoginForm component', () => {
     await user.type(usernameInput, 'wronguser');
     await user.type(passwordInput, 'wrongpass');
 
-    await act(async () => {
-      await user.click(loginButton);
-    });
+    await user.click(loginButton);
 
     await waitFor(() => {
       expect(
