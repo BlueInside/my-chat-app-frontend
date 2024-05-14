b199ab48c2e579fa5c10fbf7a636d4c55e7383c0
Karol <kpulawski13@gmail.com>
2024-05-14 16:14:11 +0100

    Use users from loaderData and render UsersDisplay

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,22 @@
-import { Form } from 'react-router-dom';
-
+import { Form, useLoaderData } from 'react-router-dom';
+import UsersDisplay from './UsersDisplay';
 export default function SearchBar() {
+  const { users, q } = useLoaderData();
+
   return (
-    <Form id="search-form" role="search">
-      <input
-        id="q"
-        aria-label="Search conversation"
-        placeholder="Search..."
-        type="search"
-        name="q"
-      />
-      <div id="search-spinner" aria-hidden hidden={true}></div>
-    </Form>
+    <>
+      <Form id="search-form" role="search">
+        <input
+          id="q"
+          aria-label="Search conversation"
+          placeholder="Search..."
+          type="search"
+          name="q"
+          defaultValue={q}
+        />
+        <div id="search-spinner" aria-hidden hidden={true}></div>
+      </Form>
+      <UsersDisplay users={users} />
+    </>
   );
 }
