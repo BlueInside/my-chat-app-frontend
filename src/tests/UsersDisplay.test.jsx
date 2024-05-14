cf655cd079a9f7bab741850066702a8b6ec29fb0
Karol <kpulawski13@gmail.com>
2024-05-14 21:21:23 +0100

    Create test that checks if api call was fired on user item click

--- src/tests/UsersDisplay.test.jsx
+++ src/tests/UsersDisplay.test.jsx
@@ -1,7 +1,12 @@
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 import UsersDisplay from '../components/UsersDisplay';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
+const mockAxios = new MockAdapter(axios);
 
 let router;
 describe('UsersDisplay component', () => {
@@ -45,5 +50,35 @@ describe('UsersDisplay component', () => {
     render(<RouterProvider router={router} />);
     const users = screen.queryAllByRole('listitem');
     expect(users).toHaveLength(0);
+  });
+
+  it('Creates or fetches a conversation when a user item is clicked', async () => {
+    mockAxios.onPost('http://localhost:3000/conversations').reply(200, {
+      conversation: { id: '123', participants: ['user1', 'user2'] },
+    });
+    const user = userEvent.setup();
+
+    const mockUsers = [{ _id: 'user2', username: 'TestUser' }];
+
+    router = createMemoryRouter(
+      [
+        {
+          path: '/',
+          element: <UsersDisplay users={mockUsers} />,
+        },
+      ],
+      { initialEntries: ['/'] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    await user.click(screen.getByText(/testUser/i));
+
+    await waitFor(() => {
+      expect(mockAxios.history.post.length).toBe(1);
+      expect(mockAxios.history.post[0].url).toBe(
+        'http://localhost:3000/conversations'
+      );
+    });
   });
 });
