5f3c658506685571e5aceba80d980f9ba7e0755e
Karol <kpulawski13@gmail.com>
2024-05-14 16:27:54 +0100

    Remove console.logs

--- src/api/users.js
+++ src/api/users.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 const usersLoader = async ({ request }) => {
   try {
     const url = new URL(request.url);
-    console.log(url);
     const q = url.searchParams.get('q');
     const response = await axios.get(`http://localhost:3000/users`, {
       params: { q: q },
     });
-    console.log(q);
     return { users: response.data.users, q };
   } catch (error) {
     console.error('Failed to fetch users', error);
