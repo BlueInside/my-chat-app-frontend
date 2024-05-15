9ec5cc6ebb589247c7516311cde980492f01e809
Karol <kpulawski13@gmail.com>
2024-05-15 08:31:16 +0100

    Test that checks if setConversations was called

--- src/tests/UsersDisplay.test.jsx
+++ src/tests/UsersDisplay.test.jsx
@@ -136,4 +136,40 @@ describe('UsersDisplay component', () => {
       ).toBeInTheDocument();
     });
   });
+
+  it('Should call setConversation when successfully created', async () => {
+    const mockSetConversations = vi.fn();
+
+    mockAxios.onPost('http://localhost:3000/conversations').reply(200, {
+      conversation: { id: '123', participants: ['user1', 'user2'] },
+    });
+
+    const user = userEvent.setup();
+
+    const mockUsers = [{ _id: 'user2', username: 'TestUser' }];
+
+    router = createMemoryRouter(
+      [
+        {
+          path: '/',
+          element: (
+            <UsersDisplay
+              setConversations={mockSetConversations}
+              conversations={[]}
+              users={mockUsers}
+            />
+          ),
+        },
+      ],
+      { initialEntries: ['/'] }
+    );
+
+    render(<RouterProvider router={router} />);
+
+    await user.click(screen.getByText(/testUser/i));
+
+    await waitFor(() => {
+      expect(mockSetConversations).toHaveBeenCalledOnce();
+    });
+  });
 });
