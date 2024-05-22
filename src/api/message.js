abc2ca618d2f8820af6783a6a328063d1a0accd2
Karol <kpulawski13@gmail.com>
2024-05-22 16:27:18 +0100

    Moved localStorage inside messageAction call

--- src/api/message.js
+++ src/api/message.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-const token = localStorage.getItem('token');
 
 const messageAction = async ({ request }) => {
+  const token = localStorage.getItem('token');
   if (!token) {
     throw new Response('Authentication required', { status: 401 });
   }
