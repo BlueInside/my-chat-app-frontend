a7b33d81f2c8e84330e51c1b1d411e934a81590f
Karol <kpulawski13@gmail.com>
2024-05-23 08:51:13 +0100

    Create UserProfileHeader

---
+++ src/tests/unit/UserProfileHeader.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import UserProfileHeader from '../../components/UserProfileHeader';
+
+describe('UserProfileHeader component', () => {
+  it('Should display user username and show user avatar', async () => {
+    render(<UserProfileHeader username={'Karol'} avatarUrl={'#'} />);
+
+    const avatar = screen.getByRole('img', { name: 'avatar' });
+
+    expect(avatar).toBeInTheDocument();
+    expect(avatar).toHaveAttribute('src', '#');
+    expect(screen.getByText('Karol')).toBeInTheDocument();
+  });
+});
