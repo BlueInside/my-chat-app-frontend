418c7674e024a40d5c6e57c065da4ee8623a76d9
Karol <kpulawski13@gmail.com>
2024-05-23 20:09:09 +0100

    Test ProfilePage

--- src/tests/integration/ProfilePage.test.jsx
+++ src/tests/integration/ProfilePage.test.jsx
@@ -1,32 +1,66 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import ProfilePage from '../../pages/ProfilePage';
 
-let mockUserData = {
+const mockUseAuth = vi.fn();
+vi.mock('../../utils/AuthContext', () => ({
+  useAuth: () => mockUseAuth(),
+}));
+
+const mockUserData = {
   _id: 'user1',
   username: 'Karol',
   fullName: 'Karol Pulawski',
   bio: 'My very interesting bio',
   avatarUrl: '#',
-  dateOfBirth: new Date('1997 05 18'),
+  dateOfBirth: '1997 05 18',
 };
 
-let router = createMemoryRouter(
+const router = createMemoryRouter(
   [
     {
       path: '/',
       element: <ProfilePage />,
       loader: () => mockUserData,
     },
+    { path: 'edit', element: <div>Edit profile page</div> },
   ],
   { initialEntries: ['/'] }
 );
 
 describe('ProfilePage Component', async () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   it('Renders Profile page properly', () => {
+    mockUseAuth.mockReturnValue({ user: { id: 'user1' } });
     const { container } = render(<RouterProvider router={router} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it(`Should display edit button when current user id is same as profile param id`, async () => {
+    mockUseAuth.mockReturnValue({ user: { id: 'user1' } });
+    render(<RouterProvider router={router} />);
+
+    const editProfileButton = screen.getByRole('link', {
+      name: /edit profile/i,
+    });
+
+    expect(editProfileButton).toBeInTheDocument();
+  });
+
+  it(`Shouldn't display edit button when current user id is different than profile param id`, async () => {
+    mockUseAuth.mockReturnValue({ user: { id: 'user2' } });
+
+    render(<RouterProvider router={router} />);
+
+    const editProfileButton = screen.queryByRole('link', {
+      name: /edit profile/i,
+    });
+
+    expect(editProfileButton).not.toBeInTheDocument();
   });
 });
