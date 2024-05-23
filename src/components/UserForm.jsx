edf4d340f5e58429dd469d6974abdcbc6d485f10
Karol <kpulawski13@gmail.com>
2024-05-23 08:49:49 +0100

    Create UserForm component

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -4,7 +4,38 @@ export default function UserForm() {
   const fetcher = useFetcher();
   return (
     <div>
-      <fetcher.Form></fetcher.Form>
+      <fetcher.Form>
+        <div>
+          <label htmlFor="fullName">Full name</label>
+          <input
+            id="fullName"
+            name="fullName"
+            type="text"
+            aria-label="Full name"
+          />
+        </div>
+        <div>
+          <label htmlFor="avatar">Avatar</label>
+          <input
+            id="avatar"
+            name="avatar"
+            type="file"
+            aria-label="Avatar"
+            placeholder="Choose a profile picture"
+          />
+        </div>
+        <div>
+          <label htmlFor="dateOfBirth">Date of Birth</label>
+          <input
+            id="dateOfBirth"
+            name="dateOfBirth"
+            type="date"
+            aria-label="Date of Birth"
+            placeholder="Enter your date of birth"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </fetcher.Form>
     </div>
   );
 }
