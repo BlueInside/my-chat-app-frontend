aa9bc8ab573c43721c96d5f7e4b5781786af6091
Karol <kpulawski13@gmail.com>
2024-05-23 09:55:00 +0100

    Implement userLoader

--- src/api/users.js
+++ src/api/users.js
@@ -14,4 +14,44 @@ const usersLoader = async ({ request }) => {
   }
 };
 
-export { usersLoader };
+const userLoader = async ({ params }) => {
+  const token = localStorage.getItem('token');
+  if (!token) {
+    throw new Response('Authentication required', { status: 401 });
+  }
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/users/${params.userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log('User Loader: ', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user details:', error);
+
+    // Error handling based on the HTTP status code from the backend
+    if (error.response) {
+      const { status, data } = error.response;
+      switch (status) {
+        case 404:
+          throw new Response('Conversation not found.', { status: 404 });
+        default:
+          throw new Response(
+            data.message || 'An error occurred while fetching user details.',
+            { status }
+          );
+      }
+    } else {
+      // For network errors or other issues not explicitly related to the HTTP response
+      throw new Response(
+        'Failed to connect to the server. Please check your network connection.',
+        { status: 500 }
+      );
+    }
+  }
+};
+export { usersLoader, userLoader };
