7c877303279cc674b13d597483cf04033d3fe4e3
Karol <kpulawski13@gmail.com>
2024-05-23 08:50:53 +0100

    Add mockUser and loader to ProfilePage test

--- src/tests/unit/ProfilePage.test.jsx
+++ src/tests/unit/ProfilePage.test.jsx
@@ -3,11 +3,21 @@ import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 import ProfilePage from '../../pages/ProfilePage';
 
+let mockUserData = {
+  _id: 'user1',
+  username: 'Karol',
+  fullName: 'Karol Pulawski',
+  bio: 'My very interesting bio',
+  avatarUrl: '#',
+  dateOfBirth: new Date('1997 05 18'),
+};
+
 let router = createMemoryRouter(
   [
     {
       path: '/',
       element: <ProfilePage />,
+      loader: () => mockUserData,
     },
   ],
   { initialEntries: ['/'] }
