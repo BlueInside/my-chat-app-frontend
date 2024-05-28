70fe0705edfcb703fa5faf08864ebc007bcabe2e
Karol <kpulawski13@gmail.com>
2024-05-28 08:28:11 +0100

    UserForm unit test, if display error and if handles submission

--- src/tests/unit/UserForm.test.jsx
+++ src/tests/unit/UserForm.test.jsx
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import UserForm from '../../components/UserForm';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 const mockUser = {
   _id: 'user1',
@@ -44,5 +45,60 @@ describe('UserForm component', () => {
 
     expect(screen.getByLabelText('Full name').value).toBe(mockUser.fullName);
     expect(screen.getByLabelText('Bio').value).toBe(mockUser.bio);
+  });
+
+  it('Handles form submission correctly', async () => {
+    const user = userEvent.setup();
+    render(<RouterProvider router={router} />);
+
+    await user.clear(screen.getByLabelText('Full name'));
+    await user.type(screen.getByLabelText('Full name'), 'New name');
+
+    await user.clear(screen.getByLabelText('Bio'));
+    await user.type(screen.getByLabelText('Bio'), 'New Bio');
+
+    const file = new File(['dummy content'], 'test.png', { type: 'image/png' });
+    await user.upload(screen.getByLabelText('Avatar'), file);
+
+    expect(screen.getByLabelText('Full name').value).toBe('New name');
+    expect(screen.getByLabelText('Bio').value).toBe('New Bio');
+    expect(screen.getByLabelText('Avatar').files[0]).toEqual(file);
+  });
+
+  it('Should not display error message when an valid image type is selected', async () => {
+    const user = userEvent.setup();
+
+    render(<RouterProvider router={router} />);
+    const fileInput = screen.getByLabelText('Avatar');
+    const file = new File(['(⌐□_□)'], 'test.png', { type: 'image/png' });
+
+    await user.upload(fileInput, file);
+
+    const fileInputError = screen.queryByText(
+      'Unsupported file type. Please select an image (JPEG, PNG, GIF).'
+    );
+
+    expect(fileInput.files[0].name).toBe('test.png');
+    expect(fileInput.files[0].type).toBe('image/png');
+    expect(fileInputError).not.toBeInTheDocument();
+  });
+
+  it('Should display error message when image type is invalid', async () => {
+    const user = userEvent.setup();
+
+    render(<RouterProvider router={router} />);
+
+    const fileInput = screen.getByLabelText('Avatar');
+    const unsupportedFile = new File(['content'], 'virus.exe', {
+      type: 'application/x-msdownload',
+    });
+
+    await user.upload(fileInput, unsupportedFile);
+
+    const fileInputError = screen.getByText(
+      'Unsupported file type. Please select an image (JPEG, PNG, GIF).'
+    );
+
+    expect(fileInputError).toBeInTheDocument();
   });
 });
